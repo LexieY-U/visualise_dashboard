7c404ba4454f65c7fe358ffca7d7eb0f25a9af34
LexieY-U <60623950+LexieY-U@users.noreply.github.com>
2025-03-19 11:45:11 +0000

    Update file path so it can work properly in sagemaker

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
+import os
 
 # ---- Load Data from Excel ----
 # local version 
-file_path = r"C:\Users\jiyin.yu\OneDrive - Accenture\Desktop\SOD\visualise_dashboard\SupplyChain_Exceptions_Dummy_Data.xlsx"
+# file_path = r"C:\Users\jiyin.yu\OneDrive - Accenture\Desktop\SOD\visualise_dashboard\SupplyChain_Exceptions_Dummy_Data.xlsx"
 # Remote version 
 # file_path = "/home/ec2-user/environment/.c9/visualise_dashboard/SupplyChain_Exceptions_Dummy_Data.xlsx"
+
+# Get current script directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Define correct file path (relative to script location)
+file_path = os.path.join(current_dir, "SupplyChain_Exceptions_Dummy_Data.xlsx")
+
 # Read Excel file into pandas DataFrame
 # print(df.dtypes)
 def load_data():
