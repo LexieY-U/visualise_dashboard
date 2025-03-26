b472e5e17249cd09ff4fb63a83f0085e2b681702
LexieY-U <60623950+LexieY-U@users.noreply.github.com>
2025-03-26 15:34:06 +0000

    Update data_loader.py as using s3 instead of excel

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
-import os
+import boto3
+
+# Use S3 bucket
+s3 = boto3.client("s3")
+bucket_name = "supply-chain-dummy-data"
+file_name = "SupplyChain_Exceptions_Dummy_Data.xlsx"
+
+# Test S3 access in Sagemaker
+response = s3.list_objects_v2(Bucket=bucket_name)
+print(response)
 
 # ---- Load Data from Excel ----
 # local version 
@@ -8,19 +17,18 @@ import os
 # Remote version 
 # file_path = "/home/ec2-user/environment/.c9/visualise_dashboard/SupplyChain_Exceptions_Dummy_Data.xlsx"
 
-# Get current script directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Define correct file path (relative to script location)
-file_path = os.path.join(current_dir, "SupplyChain_Exceptions_Dummy_Data.xlsx")
-
-# Read Excel file into pandas DataFrame
-# print(df.dtypes)
 def load_data():
-    df = pd.read_excel(file_path, dtype=str)
-    df.columns = df.columns.str.strip()  # Clean column names
-
-    if "df" not in st.session_state:
-        st.session_state.df = df
-        st.session_state.edited_df = df.copy()
-
-    return df
+    try:
+        # Fetch file from S3
+        s3.get_object(Bucket=bucket_name, Key=file_name)
+        df = pd.read_excel(f"s3://{bucket_name}/{file_name}", dtype=str)
+        df.columns = df.columns.str.strip()  # Clean column names
+        
+        if "df" not in st.session_state:
+            st.session_state.df = df
+            st.session_state.edited_df = df.copy()
+        return df
+    
+    except Exception as e:
+        st.error(f"Error loading data from s3: {e}")
+        return None
